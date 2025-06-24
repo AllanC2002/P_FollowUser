bbe771741566e65651418780724e73a6b0554138
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-06-24 16:28:05 -0500

    test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
 
-BASE_URL_FOLLOW = "http://localhost:8080"  # Tu microservicio Go (follow)
+BASE_URL_FOLLOW = "http://54.163.143.185:8080"  # Tu microservicio Go (follow)
 BASE_URL_LOGIN = "http://52.203.72.116:8080"   # Microservicio login en Python
 
 # Datos para login
 login_data = {
-    "User_mail": "allan2",
+    "User_mail": "allan",
     "password": "1234"
 }
 
@@ -21,7 +21,7 @@ print("Token obtenido:", token)
 
 # Datos para seguir usuario (id_following debe ser el id del usuario a seguir)
 data_follow = {
-    "id_following": 3
+    "id_following": 4
 }
 
 # Headers con token para el microservicio Go
